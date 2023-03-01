8fdfbb876c38af15b0bab88aa6c0b3928c9921dd
Njabeh Delphine Fanwi <69277983+Fanwi-Afriitech@users.noreply.github.com>
2023-03-01 22:50:31 +0300

    Add files via upload

---
+++ baseH.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main() {
+	
+	cout<<"Delphine Fanwi\n";
+	
+	int a,b,c;
+	cout<<" ENTER BASE\n";
+	cin>>a;
+	
+	cout<<" ENTER height\n";
+	cin>>b;
+	
+	c=0.5*(a*b);
+	cout<<"The area is "<< "\t\t"<<c;
+	
+	
+	return 0;
+}
